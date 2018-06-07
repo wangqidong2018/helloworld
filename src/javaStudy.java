eaf14545f15186afc57f8e62a084b0e1b502b7d9
wangqidong2018 <hk.wqd@163.com>
2018-06-07 10:07:55 +0800

    first project

--- src/javaStudy.java
+++ src/javaStudy.java
@@ -6,5 +6,6 @@ public class javaStudy {
     public static void main(String[] args){
 
         System.out.println("hello world" + "update" + "123");
+        System.out.println("this is branch01");
     }
 }
