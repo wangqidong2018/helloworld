ec92e687504ef36c1d1d0ef731132f78d2f9a67c
wangqidong2018 <hk.wqd@163.com>
2018-06-07 10:30:24 +0800

    first project

--- src/javaStudy.java
+++ src/javaStudy.java
@@ -5,9 +5,7 @@ public class javaStudy {
 
     public static void main(String[] args){
 
-        System.out.println("hello world" + "update" + "123");
+        System.out.println("hello world 01");
         System.out.println("this is 02 branch");
-        System.out.println("this is 01 branch" + "and 02 branch message auto syn");
-        System.out.println("after branch02 commit");
     }
 }
